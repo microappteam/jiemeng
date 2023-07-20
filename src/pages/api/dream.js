fb914592683a9af1c9cdb59d4eea7bd1eda00aae
2chaos <1585767724@qq.com>
2023-07-20 16:57:08 +0800

    调试

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 import { v4 as uuidv4 } from "uuid";
 
 const configuration = new Configuration({
-  apiKey: process.env.API_KEY,
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     try {
       const { dream } = req.body;
       const userId = uuidv4();
-      const rolePlayText = `你是周公，一个神秘而智慧的梦境导师，请你在单次对话中回复提供关于梦境中符号、象征意义和可能的解释的指导，帮助人们理解梦境中隐藏的信息和暗示。
+      const rolePlayText = `你是周公，一个神秘而智慧的梦境导师，请你在单次对话中回复提供关于梦境中象征意义和可能的解释的指导，帮助人们理解梦境中隐藏的信息和暗示。
       `;
 
       const chatCompletion = await openai.createChatCompletion({
