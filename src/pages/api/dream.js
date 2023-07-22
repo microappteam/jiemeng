68e3b21d46966dc16e2e303b81b1de014ef31cf1
2chaos <1585767724@qq.com>
2023-07-22 17:34:08 +0800

    将temperature改为1

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
           { role: "system", content: rolePlayText },
           { role: "user", content: `UserId: ${userId}\n${dream}` },
         ],
-        temperature: 0.8,
+        temperature: 1,
         max_tokens: 400,
       });
 
