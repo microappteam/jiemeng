ecab71b1c6bb75c2a288eb67032000a8a1160ee5
2chaos <1585767724@qq.com>
2023-08-03 23:06:24 +0800

    使用StyledResponse

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,43 @@ import { useState } from "react";
 import axios from "axios";
 import ReactMarkdown from "react-markdown";
 import Head from "next/head";
-import { App, Button, Input, ConfigProvider } from "antd";
-import zhCN from "antd/lib/locale/zh_CN";
+import { Button, Input } from "antd";
+import styled from "styled-components"; // Import styled-components
+
 const { TextArea } = Input;
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: #fffbe9;
+  height: 100vh;
+  padding-top: 20px;
+`;
+
+const StyledContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 12px;
+  text-align: center;
+  width: 90%;
+  max-width: 400px;
+`;
+
+const StyledResponse = styled.div`
+  margin-top: 20px;
+  text-align: left;
+`;
+
+const StyledResponseText = styled.p`
+  font-size: 16px;
+  background-color: #e3caa5;
+  white-space: pre-line;
+  overflow-wrap: break-word;
+`;
+
 export default function Home() {
   const [dream, setDream] = useState("");
   const [response, setResponse] = useState("");
@@ -24,94 +58,51 @@ export default function Home() {
   };
 
   return (
-    <App>
-      <ConfigProvider
-        locale={zhCN}
-        getPopupContainer={() => contentRef.current}
-      >
-        <div className="container">
-          <Head>
-            <title>周公解梦</title>
-            <link rel="icon" href="/dream.png" />
-          </Head>
-          <div className="content">
-            <h1 className="title">周公解梦</h1>
-            <form onSubmit={handleSubmit}>
-              <TextArea
-                style={{
-                  borderColor: "#CEAB93",
-                  borderWidth: "1px",
-                  width: 300,
-                }}
-                value={dream}
-                showCount
-                rows={5}
-                maxLength={400}
-                placeholder="请输入梦境"
-                onChange={(e) => setDream(e.target.value)}
-              />
-              <br />
-              <br />
+    <StyledContainer>
+      <Head>
+        <title>周公解梦</title>
+        <link rel="icon" href="/dream.png" />
+      </Head>
+      <StyledContent>
+        <h1 className="title">周公解梦</h1>
+        <form onSubmit={handleSubmit}>
+          <TextArea
+            style={{
+              borderColor: "#CEAB93",
+              borderWidth: "1px",
+              width: 300,
+            }}
+            value={dream}
+            showCount
+            rows={5}
+            maxLength={400}
+            placeholder="请输入梦境"
+            onChange={(e) => setDream(e.target.value)}
+          />
+          <br />
+          <br />
 
-              <Button
-                block
-                size="large"
-                style={{
-                  backgroundColor: "#CEAB93",
-                  borderColor: "#CEAB93",
-                  borderWidth: "1px",
-                  color: "#000",
-                }}
-                onClick={handleSubmit}
-              >
-                解梦
-              </Button>
-            </form>
-            {response && (
-              <div className="response">
-                <p>解梦结果：</p>
-                <ReactMarkdown className="response-text">
-                  {response}
-                </ReactMarkdown>
-              </div>
-            )}
-          </div>
-
-          <style jsx>{`
-            .container {
-              display: flex;
-              justify-content: center;
-              align-items: flex-start;
-              background-color: #fffbe9;
-              height: 100vh;
-              padding-top: 20px;
-            }
-
-            .content {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              flex-direction: column;
-              padding: 12px;
-              text-align: center;
-              width: 90%;
-              max-width: 400px;
-            }
-
-            .response {
-              margin-top: 20px;
-              text-align: left;
-            }
-
-            .response-text {
-              font-size: 16px;
-              background-color: #e3caa5;
-              white-space: pre-line;
-              overflow-wrap: break-word;
-            }
-          `}</style>
-        </div>
-      </ConfigProvider>
-    </App>
+          <Button
+            block
+            size="large"
+            style={{
+              backgroundColor: "#CEAB93",
+              borderColor: "#CEAB93",
+              borderWidth: "1px",
+              color: "#000",
+            }}
+            onClick={handleSubmit}
+          >
+            解梦
+          </Button>
+        </form>
+        {response && (
+          <StyledResponse>
+            <p>解梦结果：</p>
+            <StyledResponseText>{response}</StyledResponseText>
+          </StyledResponse>
+        )}
+      </StyledContent>
+    </StyledContainer>
   );
 }
