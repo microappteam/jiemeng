58e1464e0d917bf669f13fb8c5c963fa542254d0
2chaos <1585767724@qq.com>
2023-08-04 10:02:23 +0800

    将组件拆分

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,107 +1,33 @@
-import { useState, useEffect } from "react";
+// index.js
+import { useState } from "react";
 import axios from "axios";
 import Head from "next/head";
-import { Button, Input } from "antd";
-import styled from "styled-components";
-
-const { TextArea } = Input;
-
-const StyledContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  background-color: #fffbe9;
-  height: 100vh;
-  padding-top: 20px;
-`;
-
-const StyledContent = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 12px;
-  text-align: center;
-  width: 90%;
-  max-width: 400px;
-`;
-
-const StyledResponse = styled.div`
-  margin-top: 20px;
-  text-align: left;
-`;
-
-const StyledResponseText = styled.p`
-  font-size: 16px;
-  white-space: pre-line;
-  overflow-wrap: break-word;
-`;
-
-const StyledButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin-top: 16px;
-`;
-
-const StyledButton = styled(Button)`
-  background-color: #ceab93;
-  border-color: #ceab93;
-  border-width: 1px;
-  color: #000;
-
-  &:hover,
-  &:focus {
-    background-color: #ceab93;
-    border-color: #ceab93;
-    color: #000;
-  }
-
-  &:active {
-    background-color: #ceab93;
-    border-color: #ceab93;
-    color: #000;
-    box-shadow: none;
-  }
-
-  &:disabled {
-    background-color: #ceab93;
-    border-color: #ceab93;
-    color: #000;
-    opacity: 0.7;
-  }
-`;
-
-const loadingMessages = [
-  "Loading...",
-  "正在询问周公...",
-  "正在翻阅梦书...",
-  "好运正在路上...",
-  "Loading 101% ...",
-  "慢工出细活，久久方为功...",
-  "周公正在解读梦境，请稍候...",
-  "加载中，请稍候...",
-  "卖力加载中...",
-  "O.o ...",
-  "马上就要写完咯...",
-];
+import { App, ConfigProvider } from "antd";
+import zhCN from "antd/lib/locale/zh_CN";
+import StyledComponentsRegistry from "./registry";
 
 export default function Home() {
   const [dream, setDream] = useState("");
   const [response, setResponse] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [randomMessage, setRandomMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    if (loading) {
-      const randomIndex = Math.floor(Math.random() * loadingMessages.length);
-      setRandomMessage(loadingMessages[randomIndex]);
-    }
-  }, [loading]);
+  const loadingTexts = [
+    "Loading...",
+    "正在询问周公...",
+    "正在翻阅梦书...",
+    "好运正在路上...",
+    "Loading 101% ...",
+    "慢工出细活，久久方为功...",
+    "周公正在解读梦境，请稍候...",
+    "加载中，请稍候...",
+    "卖力加载中...",
+    "O.o ...",
+    "马上就要写完咯...",
+  ];
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+    setIsLoading(true);
     try {
       const response = await axios.post(
         "/api/dream",
@@ -112,53 +38,39 @@ export default function Home() {
     } catch (error) {
       console.error(error);
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
   };
 
   return (
-    <StyledContainer>
-      <Head>
-        <title>周公解梦</title>
-        <link rel="icon" href="/dream.png" />
-      </Head>
-      <StyledContent>
-        <h1 className="title">周公解梦</h1>
-        <form onSubmit={handleSubmit}>
-          <TextArea
-            style={{
-              borderColor: "#CEAB93",
-              borderWidth: "1px",
-              width: 300,
-            }}
-            value={dream}
-            showCount
-            rows={5}
-            maxLength={400}
-            placeholder="请输入梦境"
-            onChange={(e) => setDream(e.target.value)}
+    <App>
+      <ConfigProvider locale={zhCN}>
+        <div className="container">
+          <Head>
+            <title>周公解梦</title>
+            <link rel="icon" href="/dream.png" />
+          </Head>
+          <StyledComponentsRegistry
+            dream={dream}
+            setDream={setDream}
+            handleSubmit={handleSubmit}
+            response={response}
+            isLoading={isLoading}
+            loadingTexts={loadingTexts}
           />
-          <br />
-          <br />
+        </div>
+      </ConfigProvider>
 
-          <StyledButtonContainer>
-            <StyledButton
-              block
-              size="large"
-              onClick={handleSubmit}
-              disabled={loading}
-            >
-              {loading ? randomMessage : "解梦"}
-            </StyledButton>
-          </StyledButtonContainer>
-        </form>
-        {response && (
-          <StyledResponse>
-            <p>解梦结果：</p>
-            <StyledResponseText>{response}</StyledResponseText>
-          </StyledResponse>
-        )}
-      </StyledContent>
-    </StyledContainer>
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          align-items: flex-start;
+          background-color: #fffbe9;
+          height: 100vh;
+          padding-top: 20px;
+        }
+      `}</style>
+    </App>
   );
 }
