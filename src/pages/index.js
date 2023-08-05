177519f3e5b5297700ef5a7b9cc2ee5101dfeef8
期贤 <qixian.cs@outlook.com>
2023-08-05 20:27:36 +0800

    优化 title 兼容

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,6 +52,16 @@ export default function Home() {
             <meta property="og:title" content="周公解梦"></meta>
             <meta property="twitter:image" content="/logo.png"></meta>
             <meta property="og:image" content="/logo.png"></meta>
+            <meta property="twitter:title" content="周公解梦"></meta>
+            <meta property="twitter:card" content="summary"></meta>
+            <meta
+              property="twitter:description"
+              content="周公解梦是一种将梦境解读为暗示和预兆的传统文化实践。在中国古代，人们相信梦境可以透露出隐藏的信息或未来事件。因此，他们会寻求有经验的解梦师（如周公）来帮助理解和分析自己的梦境。"
+            ></meta>
+            <meta
+              property="og:url"
+              content="https://jiemeng.chenshuai.dev"
+            ></meta>
             <meta
               property="og:description"
               content="周公解梦是一种将梦境解读为暗示和预兆的传统文化实践。在中国古代，人们相信梦境可以透露出隐藏的信息或未来事件。因此，他们会寻求有经验的解梦师（如周公）来帮助理解和分析自己的梦境。"
