3f33ae43f773ad86bb2ffc9eddbefb3d9fb47568
2chaos <1585767724@qq.com>
2023-08-08 00:46:38 +0800

    修改api写法

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,30 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+const NextAuth = require('next-auth');
+const GithubProvider = require('next-auth/providers/github');
 
-const options = {
+const authOptions = {
+  secret: process.env.NEXT_AUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user = { ...session.user, ...user };
+      }
+      return session;
+    },
+  },
   providers: [
-    Providers.GitHub({
+    GithubProvider({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.login,
+          name: profile.name ?? profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
     }),
   ],
 };
 
-export default NextAuth(options);
+module.exports = NextAuth(authOptions);
