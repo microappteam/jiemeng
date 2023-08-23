65db5659e9fdc84b8670855de8bc553337bd2996
2chaos <1585767724@qq.com>
2023-08-23 23:20:04 +0800

    使用pg库连接到数据库

--- src/pages/api/storage.js
+++ src/pages/api/storage.js
@@ -1,35 +1,40 @@
-import { Client } from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from 'pg';
+require('dotenv').config();
 
-dotenv.config();
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
 export default async function handler(req, res) {
-  try {
-    await client.connect();
+  if (req.method === 'POST') {
+    const { dream } = req.body;
+    const { username } = req.session.user;
 
-    const { dream, username, response } = req.body;
+    try {
+      const client = await pool.connect();
+      const query =
+        'INSERT INTO dreams (dream, user_id, username) VALUES ($1, $2, $3) RETURNING *';
+      const values = [dream, req.session.userId, username];
+      const result = await client.query(query, values);
+      client.release();
 
-    const query = `
-      INSERT INTO dreams (dream, username, response)
-      VALUES ($1, $2, $3)
-      RETURNING *
-    `;
-    const values = [dream, username, response];
+      res.status(200).json(result.rows[0]);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  } else if (req.method === 'GET') {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM dreams');
+      client.release();
 
-    const result = await client.query(query, values);
-
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Failed to store data.' });
-  } finally {
-    await client.end();
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
