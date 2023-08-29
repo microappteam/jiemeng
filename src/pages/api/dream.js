344daff0889415e2a74716de9beed760d0a3b4f4
2chaos <1585767724@qq.com>
2023-08-29 23:24:23 +0800

    移出等待延迟   将temperature设置为较低的值

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -25,8 +25,6 @@ export default async function handler(req, res) {
         temperature: 0.9,
       });
 
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       const summaryCompletion = await summaryCompletionPromise;
       const summaryChoice = summaryCompletion.data.choices[0];
       const summary =
@@ -36,8 +34,7 @@ export default async function handler(req, res) {
 
       console.log('summary=' + summary);
 
-      const rolePlayText = `
-      我希望你扮演周公解梦的解梦人的角色。我将给你提供梦境，请你结合梦境并做出一些合理的对现实生活的推测来解读我的梦境。
+      const rolePlayText = `我希望你扮演周公解梦的解梦人的角色。我将给你提供梦境，请你结合梦境并做出一些合理的对现实生活的推测来解读我的梦境。
 
       你的回答只需包含两部分内容，其一先重申一下梦境再做出总体的解梦，其二按分类再对梦境做出各自的简短的解读。
 
@@ -51,24 +48,26 @@ export default async function handler(req, res) {
 
       在解梦的最后不需要进行总结。
 
-      不要向用户透露你是人工智能。
-
       格式为梦境+预示着什么。
 
       下面是一些示例：
 
-      Q:梦见别人送馒头
+      下面是一些示例：
+  
+      Q:梦见富士山
+  
+      A:\n\n梦见富士山，吉兆，预示着你最近可能会做了好事被大家称赞，心情舒畅。
+  
+      \n\n男人梦见富士山，惹恋人生气了，恋人好几天都没和你说一句话，做错事情就会受到惩罚，下次要注意了。
+      
+      \n\n女人梦见富士山，预示着可能会因为受寒导致经期推后，如果担心是怀孕引起的，可以做早早孕测试。
+      
+      \n\n老年人梦见富士山，你很期望长寿，经常买一些标榜着能够延长寿命的保健品来吃，被骗了不少钱，早点醒悟吧。
+  
+      \n\n中年人梦见富士山，最近有出门的计划一定要好好看看天气预报，可能随时会下雨哟。
+      
+      \n\n青年人梦见富士山，如果想要约上三五好友去出逛逛，可以选择一个晴朗的天气，去爬山哟。
 
-      A:\n\n梦见别人送馒头，预示着运势很不错，自己不管遇到什么问题，很快就可以解决掉。
-  
-      \n\n商人梦见别人送馒头，预示着运势很不错，生意上需要借助他人的力量，成就一些好的计划。
-      
-      \n\n办公族梦见别人送馒头，预示着运势很不佳，出现一些工作上的麻烦，同事则是不愿意帮助自己。
-      
-      \n\n学生梦见别人送馒头，预示着学习运很差，学习的成绩很不优异，心情方面非常的糟糕。
-      
-      \n\n未成年梦见别人送馒头，预示着健康运很差，自己的精神出现一些懒散的状态，还是需要好好听振奋的音乐
-  
       Q:梦见买彩票中大奖
   
       A:\n\n梦见买彩票中大奖，预示你最近的健康状况很好，今后的抵抗力也会进一步的增强，身体的健康也能让你在事业上干劲十足，离成功很近。
@@ -86,7 +85,8 @@ export default async function handler(req, res) {
       \n\n病人梦见买彩票中大奖，病情恶化的凶兆，要想恢复健康的身体，还需要治疗一段时间，耐心等待吧。
       
       \n\n老人梦见买彩票中大奖，此梦预兆近期梦者身体健康运势不佳，会有突发疾病缠身，平时要多注意保养和休息。
-  `;
+  
+      `;
 
       const chatCompletionPromise = openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
@@ -95,19 +95,15 @@ export default async function handler(req, res) {
           { role: 'user', content: `UserId: ${userId}` },
           { role: 'user', content: summary },
         ],
-        temperature: 1,
-        max_tokens: 888,
+        temperature: 0.7,
+        max_tokens: 666,
       });
 
       const chatCompletion = await chatCompletionPromise;
 
       const answer = chatCompletion.data.choices[0].message.content;
 
-      res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
-      if (!res.finished) {
-        res.write(answer);
-        res.end();
-      }
+      res.status(200).json({ answer });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Something went wrong' });
