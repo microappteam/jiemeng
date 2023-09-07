324d410033887f029de73e0ab71f4b838929db51
2chaos <1585767724@qq.com>
2023-09-07 11:02:13 +0800

    修改api

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -14,7 +14,8 @@ export default async function handler(req, res) {
     try {
       const { dream } = req.body;
       const userId = uuidv4();
-      const summaryText = `你需要将我给你的梦境进行总结，去掉一些修饰词，保留句子的谓语和宾语。`;
+      const summaryText =
+        '你需要将我给你的梦境进行总结，去掉一些修饰词，保留句子的谓语和宾语。';
       const summaryCompletionPromise = openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
@@ -37,34 +38,34 @@ export default async function handler(req, res) {
 
       console.log('summary=' + summary);
 
-      const chatPayload = {
-        prompt: summary,
-        temperature: 1,
-        max_tokens: 888,
-        stream: true,
-        stop: ['\n\n'],
-      };
+      const rolePlayText = '';
 
-      const source = new SSE(
+      const sse = new SSE(
         'https://api.openai.com/v1/engines/curie/completions',
         {
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.API_KEY}`,
+            Authorization: process.env.API_KEY,
           },
           method: 'POST',
-          payload: JSON.stringify(chatPayload),
+          payload: JSON.stringify({
+            prompt: summary,
+            temperature: 1,
+            max_tokens: 888,
+            stream: true,
+            stop: ['\n\n'],
+          }),
         },
       );
 
-      source.addEventListener('message', (event) => {
-        const payload = JSON.parse(event.data);
-        const answer = payload.choices[0].message.content;
-
-        res.status(200).json(answer);
+      sse.addEventListener('message', function (e) {
+        // Assuming we receive JSON-encoded data payloads:
+        const payload = JSON.parse(e.data);
+        console.log(payload);
+        res.status(200).json(payload);
       });
 
-      source.stream(res);
+      sse.stream();
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Something went wrong' });
