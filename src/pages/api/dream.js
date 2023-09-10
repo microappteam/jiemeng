c17900ef0c5ec4a87c9d826d372fd5080f3905f6
2chaos <1585767724@qq.com>
2023-09-10 23:27:24 +0800

    精简提示词

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -1,27 +1,63 @@
-import { create } from 'axios';
+import { Configuration, OpenAIApi } from 'openai';
+import { v4 as uuidv4 } from 'uuid';
 
-require('dotenv').config();
-
-const apiKey = process.env.OPENAI_API_KEY;
-
-const client = create({
-  headers: {
-    Authorization: 'Bearer ' + apiKey,
-  },
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+  timeout: 60000,
 });
 
-const params = {
-  prompt: 'How are you?',
-  model: 'gpt-3.5-turbo',
-  max_tokens: 10,
-  temperature: 0,
-};
+const openai = new OpenAIApi(configuration);
 
-client
-  .post('https://api.openai.com/v1/completions', params)
-  .then((result) => {
-    console.log(result.data.choices[0].text);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { dream } = req.body;
+      const userId = uuidv4();
+      const summaryText = `你需要将我给你的梦境进行总结，去掉一些修饰词，保留句子的谓语和宾语。`;
+      const summaryCompletionPromise = openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: summaryText },
+          { role: 'user', content: `UserId: ${userId}` },
+          { role: 'user', content: dream },
+        ],
+        max_tokens: 35,
+        temperature: 0.9,
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      const summaryCompletion = await summaryCompletionPromise;
+      const summaryChoice = summaryCompletion.data.choices[0];
+      const summary =
+        summaryChoice && summaryChoice.message && summaryChoice.message.content
+          ? summaryChoice.message.content.trim()
+          : '';
+
+      console.log('summary=' + summary);
+
+      const rolePlayText = ` `;
+
+      const chatCompletionPromise = openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: rolePlayText },
+          { role: 'user', content: `UserId: ${userId}` },
+          { role: 'user', content: summary },
+        ],
+        temperature: 1,
+        max_tokens: 888,
+      });
+
+      const chatCompletion = await chatCompletionPromise;
+
+      const answer = chatCompletion.data.choices[0].message.content;
+      res.status(200).json(answer);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
