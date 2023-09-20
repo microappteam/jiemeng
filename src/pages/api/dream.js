e6d1283361e795926d06840f0070ba27d6a6d922
2chaos <1585767724@qq.com>
2023-09-20 23:19:42 +0800

    重置

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -1,5 +1,12 @@
-import { ChatOpenAI } from '@openai/api';
+import { Configuration, OpenAIApi } from 'openai';
 import { v4 as uuidv4 } from 'uuid';
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+  timeout: 60000,
+});
+
+const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -7,8 +14,7 @@ export default async function handler(req, res) {
       const { dream } = req.body;
       const userId = uuidv4();
       const summaryText = `你需要将我给你的梦境进行总结，去掉一些修饰词，保留句子的谓语和宾语。`;
-
-      const summaryData = {
+      const summaryCompletionPromise = openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
           { role: 'system', content: summaryText },
@@ -16,81 +22,37 @@ export default async function handler(req, res) {
           { role: 'user', content: dream },
         ],
         max_tokens: 35,
-        temperature: 0.6,
-      };
-
-      const summaryResponse = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        summaryData,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.API_KEY}`,
-            'Content-Type': 'application/json',
-          },
-          timeout: 30000,
-        },
-      );
+        temperature: 0.9,
+      });
 
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      const summaryCompletion = summaryResponse.data.choices[0];
+      const summaryCompletion = await summaryCompletionPromise;
+      const summaryChoice = summaryCompletion.data.choices[0];
       const summary =
-        summaryCompletion &&
-        summaryCompletion.message &&
-        summaryCompletion.message.content
-          ? summaryCompletion.message.content.trim()
+        summaryChoice && summaryChoice.message && summaryChoice.message.content
+          ? summaryChoice.message.content.trim()
           : '';
 
       console.log('summary=' + summary);
 
       const rolePlayText = ` `;
 
-      const chatData = {
+      const chatCompletionPromise = openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
           { role: 'system', content: rolePlayText },
           { role: 'user', content: `UserId: ${userId}` },
           { role: 'user', content: summary },
         ],
-        temperature: 0.7,
+        temperature: 1,
         max_tokens: 888,
-        stream: true,
-      };
-
-      const chat = new ChatOpenAI({
-        apiKey: process.env.API_KEY,
       });
 
-      const answerStream = chat.createChatStream(chatData);
+      const chatCompletion = await chatCompletionPromise;
 
-      res.setHeader('Content-Type', 'text/event-stream');
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Connection', 'keep-alive');
-      res.setHeader('X-Accel-Buffering', 'no');
-
-      for await (const chunk of answerStream) {
-        const responseData = chunk.toString();
-        const startIndex = responseData.indexOf('{"content":"');
-        const endIndex = responseData.indexOf('"}', startIndex);
-        const content = responseData.slice(startIndex + 12, endIndex);
-
-        if (content && !content.includes('chatcmpl')) {
-          const finishReasonIndex = responseData.indexOf(
-            '"finish_reason":"stop"',
-          );
-          if (finishReasonIndex !== -1) {
-            break;
-          }
-
-          const formattedContent = content.replace(/\\n\\n/g, '\n');
-
-          res.write(formattedContent);
-
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-        }
-      }
-
-      res.end();
+      const answer = chatCompletion.data.choices[0].message.content;
+      res.status(200).json(answer);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Something went wrong' });
