aff699d0420dace3ba30b29f98f1014d73af1606
2chaos <1585767724@qq.com>
2023-10-10 23:56:47 +0800

    test

--- src/pages/api/dream.js
+++ src/pages/api/dream.js
@@ -9,6 +9,7 @@ export const config = {
   runtime: 'edge',
 };
 
+export const insertedRow = async (body) => {};
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
@@ -108,7 +109,11 @@ export default async function handler(req, res) {
           }
         },
       });
-
+      insertedRow({
+        dream,
+        response: '',
+        username: userId,
+      });
       return new Response(stream);
     } catch (error) {
       const res = new Response(
--- src/pages/api/storage.js
+++ src/pages/api/storage.js
@@ -6,6 +6,26 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
+const insertedRow = async (body) => {
+  const { dream, response, username } = body;
+
+  const client = await pool.connect();
+
+  const query =
+    'INSERT INTO dreams (dream, response, username,created_at) VALUES ($1,$2,$3,$4) RETURNING *';
+  const values = [
+    dream,
+    response,
+    username,
+    moment().tz('Asia/shanghai').format(),
+  ];
+  const result = await client.query(query, values);
+  client.release();
+  console.log('Inserted row:', result.rows[o]);
+  return result;
+};
+
+export { insertedRow };
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { dream, response, username } = req.body;
@@ -34,7 +54,18 @@ export default async function handler(req, res) {
       console.error('Error executing query:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+  } else if (req.method === 'GET') {
+    try {
+      const client = await pool.connect();
+      const query = 'SELECT * FROM dreams';
+      const result = await client.query(query);
+
+      client.release();
+
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
 }
