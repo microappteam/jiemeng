482032d8c35c7c85bbedbfa1524299b4cd00ce9b
2chaos <1585767724@qq.com>
2023-10-13 23:58:31 +0800

    在 storage.js 中调用写好的方法  在 index.js 的 protable 标签中添加分页  实现数据删除

--- src/pages/api/storage.js
+++ src/pages/api/storage.js
@@ -6,19 +6,24 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
-export const insertedRow = async (body) => {
+export const insertRow = async (body) => {
   const { dream, response, username } = body;
 
   const client = await pool.connect();
 
   const query =
-    'INSERT INTO dreams (dream, response, username,created_at) VALUES ($1,$2,$3,$4) RETURNING *';
+    'INSERT INTO dreams (dream, response, username, created_at) VALUES ($1, $2, $3, $4) RETURNING *';
   const values = [
     dream,
     response,
     username,
-    moment().tz('Asia/shanghai').format(),
+    moment().tz('Asia/Shanghai').format(),
   ];
+  const result = await client.query(query, values);
+
+  client.release();
+
+  return result.rows[0];
 };
 
 export default async function handler(req, res) {
@@ -29,20 +34,8 @@ export default async function handler(req, res) {
         message: 'Dream or response is empty',
       });
     try {
-      const client = await pool.connect();
-      const query =
-        'INSERT INTO dreams (dream, response, username, created_at) VALUES ($1, $2, $3, $4) RETURNING *';
-      const values = [
-        dream,
-        response,
-        username,
-        moment().tz('Asia/Shanghai').format(),
-      ];
-      const result = await client.query(query, values);
-
-      client.release();
-
-      res.status(200).json(result.rows[0]);
+      const insertedRow = await insertRow({ dream, response, username });
+      res.status(200).json(insertedRow);
     } catch (error) {
       console.error('Error executing query:', error);
       res.status(500).json({ error: 'Internal server error' });
@@ -56,6 +49,26 @@ export default async function handler(req, res) {
       client.release();
 
       res.status(200).json(result.rows);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  } else if (req.method === 'DELETE') {
+    const { id } = req.body;
+
+    if (!id) {
+      return res.status(400).json({ error: 'ID is required for deletion' });
+    }
+
+    try {
+      const client = await pool.connect();
+      const query = 'DELETE FROM dreams WHERE id = $1';
+      const values = [id];
+      await client.query(query, values);
+
+      client.release();
+
+      res.status(200).json({ message: 'Record deleted' });
     } catch (error) {
       console.error('Error executing query:', error);
       res.status(500).json({ error: 'Internal server error' });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Layout, ConfigProvider, Drawer, Table, Button } from 'antd';
+import { Layout, ConfigProvider, Drawer } from 'antd';
 import { ProTable } from '@ant-design/pro-components';
 import zhCN from 'antd/lib/locale/zh_CN';
+import axios from 'axios';
 import StyledComponentsRegistry from './component';
 import { useSession } from 'next-auth/react';
-import { preProcessFile } from 'typescript';
 
 const utf8Decoder = new TextDecoder('utf-8');
 
@@ -12,6 +12,7 @@ const params = {
   pageSize: 10,
   current: 1,
 };
+
 export default function Home() {
   const [dream, setDream] = useState('');
   const [responseText, setResponseText] = useState('');
@@ -117,16 +118,6 @@ export default function Home() {
         ...dreamHistory,
         { dream, response: tempText },
       ]);
-      const response2 = await axios.post(
-        `/api/storage`,
-        {
-          dream,
-          response: responseText,
-          username: session?.user?.name,
-        },
-        { timeout: 10000 },
-      );
-      console.log('response2', response2.data);
     } catch (error) {
       console.error(error);
     } finally {
@@ -134,10 +125,18 @@ export default function Home() {
     }
   };
 
-  const handleDelete = (index) => {
-    const newDreamHistory = [...dreamHistory];
-    newDreamHistory.splice(index, 1);
-    setDreamHistory(newDreamHistory);
+  const handleDelete = async (index) => {
+    const dreamRecord = dreamHistory[index];
+
+    try {
+      await axios.delete(`/api/storage/${dreamRecord.id}`);
+
+      const newDreamHistory = [...dreamHistory];
+      newDreamHistory.splice(index, 1);
+      setDreamHistory(newDreamHistory);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const showDrawer = () => {
@@ -164,20 +163,24 @@ export default function Home() {
               >
                 <ProTable
                   params={params}
-                  request={async (
-                    // 第一个参数 params 查询表单和 params 参数的结合
-                    // 第一个参数中一定会有 pageSize 和  current ，这两个参数是 antd 的规范
-                    params,
-                  ) => {
-                    // 这里需要返回一个 Promise,在返回之前你可以进行数据转化
-                    // 如果需要转化参数可以在这里进行修改
-                    const msg = await fetch('/api/storage', { method: 'GET' });
-                    return {
-                      data: msg,
-                      // success 请返回 true，
-                      // 不然 table 会停止解析数据，即使有数据
-                      success: true,
-                    };
+                  request={async (params) => {
+                    const response = await fetch('/api/storage', {
+                      method: 'GET',
+                    });
+                    if (response.status === 200) {
+                      const data = await response.json();
+                      return {
+                        data: data,
+                        success: true,
+                        total: data.length,
+                      };
+                    } else {
+                      return {
+                        data: [],
+                        success: false,
+                        total: 0,
+                      };
+                    }
                   }}
                   columns={[
                     {
@@ -201,7 +204,7 @@ export default function Home() {
                     },
                   ]}
                   dataSource={dreamHistory}
-                  rowKey="dream"
+                  rowKey="id"
                 />
               </Drawer>
 
