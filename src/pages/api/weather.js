7b5253795162b2bea71b119c4267e1f26468347d
2chaos <1585767724@qq.com>
2023-10-20 23:37:58 +0800

    重置 weather.js

--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -5,34 +5,27 @@ export const config = {
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-
-          console.log('您的纬度是: ' + latitude);
-          console.log('您的经度是: ' + longitude);
-        });
-      } else {
-        console.log('浏览器不支持地理位置信息获取');
-      }
       const encoder = new TextEncoder();
       const stream = new ReadableStream({
         async start(controller) {
           try {
             const response1 = await fetch(
-              'https://restapi.amap.com/v3/weather/weatherInfo?key=6c1146b9f46f7b3ca27878e074ffa4f2&city=320102',
+              'https://restapi.amap.com/v3/weather/weatherInfo?key=6c1146b9f46f7b3ca27878e074ffa4f2&city=310000',
             );
 
             const currentWeather = await response1.json();
 
             controller.enqueue(encoder.encode(JSON.stringify(currentWeather)));
 
+            console.log('当前天气  ', currentWeather);
+
             const response2 = await fetch(
-              'https://restapi.amap.com/v3/weather/weatherInfo?key=6c1146b9f46f7b3ca27878e074ffa4f2&city=320102&extensions=all',
+              'https://restapi.amap.com/v3/weather/weatherInfo?key=6c1146b9f46f7b3ca27878e074ffa4f2&city=310000&extensions=all',
             );
 
             const futureWeather = await response2.json();
+
+            console.log('未来天气  ', futureWeather);
 
             controller.enqueue(encoder.encode(JSON.stringify(futureWeather)));
 
